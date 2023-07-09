044f30a1062ab419d93ee79681a34d90b44bed5e
INCENDIOCODM <vinayakgamemaster42@gmail.com>
2023-07-09 18:03:35 +0530

    haydn: init DroidX-UI

--- droidx_haydn.mk
+++ droidx_haydn.mk
@@ -11,18 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DroidX-UI stuff.
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := haydn
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
-PRODUCT_NAME := lineage_haydn
 TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT := true
 
+PRODUCT_NAME := droidx_haydn
 PRODUCT_SYSTEM_NAME := haydn_global
 PRODUCT_SYSTEM_DEVICE := haydn
+
+# DroidX stuff
+DROIDX_BUILD_TYPE := UNOFFICIAL
+DROIDX_GAPPS := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="haydn_global-user 13 TKQ1.220829.002 V14.0.6.0.TKKMIXM release-keys" \
