6b993d1f3649d2375f5fed3a1c511f32dc2e8007
GPJ <grzegorz.jaworski1@gmail.com>
2023-08-29 21:07:59 +0200

    haydn: updating vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,22 +2,22 @@ echo 'Starting to clone stuffs needed to build for Haydn'
 
 # Device common
 echo 'Cloning Common device tree'
-git clone --depth=1 https://github.com/INCENDIOCODM/android_device_xiaomi_sm8350-common -b crDroid-13 device/xiaomi/sm8350-common
+git clone --depth=1 https://github.com/gregoorr/android_device_xiaomi_sm8350-common -b RisingOS device/xiaomi/sm8350-common
 
 # Kernel
 echo 'Cloning Kernel tree'
-git clone --depth=1 https://github.com/INCENDIOCODM/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
+git clone --depth=1 https://github.com/gregoorr/android_kernel_xiaomi_sm8350 -b lineage-20 kernel/xiaomi/sm8350
 cd kernel/xiaomi/sm8350
 git submodule init && git submodule update KernelSU
 cd ../../..
 
 # Vendor
 echo 'Cloning Vendor tree'
-git clone --depth=1 https://github.com/INCENDIOCODM/proprietary_vendor_xiaomi_haydn -b lineage-20 vendor/xiaomi/haydn
+git clone --depth=1 https://github.com/gregoorr/proprietary_vendor_xiaomi_haydn -b lineage-20 vendor/xiaomi/haydn
 
 # Vendor common
 echo 'Cloning Common vendor tree'
-git clone --depth=1 https://github.com/INCENDIOCODM/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+git clone --depth=1 https://github.com/gregoorr/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
 
 # Firmware
 echo 'Cloning Firmware'
